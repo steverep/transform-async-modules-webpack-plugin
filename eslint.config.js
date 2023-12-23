485ff08a0bb45176a44b0c7d58270ddedb6f9838
Steve Repsher <steverep@users.noreply.github.com>
2023-12-23 01:02:36 +0000

    Add ESLint globals and disable type checking on JS files

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,12 @@ import { FlatCompat } from "@eslint/eslintrc";
 import eslintJS from "@eslint/js";
 import typescriptParser from "@typescript-eslint/parser";
 import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const allFiles = "**/*.?(c|m){js,ts}";
+const jsFiles = "**/*.?(c|m)js";
 
 const rootDir = dirname(fileURLToPath(import.meta.url));
 const compat = new FlatCompat({ baseDirectory: rootDir });
@@ -19,12 +21,16 @@ export default [
       "plugin:@typescript-eslint/stylistic-type-checked",
     )
     .map((config) => ({ files: [allFiles], ...config })),
+  ...compat
+    .extends("plugin:@typescript-eslint/disable-type-checked")
+    .map((config) => ({ files: [jsFiles], ...config })),
   { files: [allFiles], ...eslintConfigPrettier },
   {
     files: [allFiles],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
+      globals: globals.nodeBuiltin,
       // Options specific to @typescript-eslint/parser
       parser: typescriptParser,
       parserOptions: {
